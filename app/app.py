0be01a04087fa9d1ffde72e065ccbb7721ce9a76
Alex Graham <agraham@evoraglobal.com>
2025-05-22 16:57:44 +0100

    Add secret that hopefully gets rejected by github on a secret scan

--- app/app.py
+++ app/app.py
@@ -5,5 +5,9 @@ def connect_to_service():
     secret_key = "AKIAFAKEKEY1234567890"
     print("Connecting with secret:", secret_key)
 
+def send_token():
+    github_token = "ghp_1234567890fakeTOKENtoTriggerScan9876543210"
+    print(github_token)
+
 if __name__ == "__main__":
     connect_to_service()
