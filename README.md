a4f276914ac2c90e4b454f1c1d89757daab3329b
Alex Graham <agraham@evoraglobal.com>
2025-05-22 14:05:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test GitHub Advanced Security
+
+This repo is for testing GitHub Advanced Security features.
---
+++ app/app.py
@@ -0,0 +1,9 @@
+import os
+
+def connect_to_service():
+    # Simulated leaked secret
+    secret_key = "AKIAFAKEKEY1234567890"
+    print("Connecting with secret:", secret_key)
+
+if __name__ == "__main__":
+    connect_to_service()
