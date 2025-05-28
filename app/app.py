937ae0dccc7a8aa79524e0b999f3b90fe406c32d
Alex Graham <agraham@evoraglobal.com>
2025-05-28 13:59:09 +0100

    more tidying - just getting to a clean version of the code that should pass all GHAS tests

--- app/app.py
+++ app/app.py
@@ -1,21 +1,21 @@
 import os
 # Triggers a GHAS leaked secret
 def connect_to_service():
-  secret_key = "AKIAFAKEKEY1234567890"
-  print("Connecting with secret:", secret_key) 
-    
-# Triggers a SonarQube Code issue
-def unused_function():
-    pass
-# Triggers a SonarQube security hotspot
-def insecure_eval(user_input):
-    eval(user_input)  
-# Triggers a SonarQube Duplication issue
-def greet_user(name):
-    print(f"Hello, {name}!")
+#   secret_key = "AKIAFAKEKEY1234567890"
+#   print("Connecting with secret:", secret_key) 
+    print("Connectibg to service") 
+# # Triggers a SonarQube Code issue
+# def unused_function():
+#     pass
+# # Triggers a SonarQube security hotspot
+# def insecure_eval(user_input):
+#     eval(user_input)  
+# # Triggers a SonarQube Duplication issue
+# def greet_user(name):
+#     print(f"Hello, {name}!")
 
-def say_hello(name):
-    print(f"Hello, {name}!")  # Duplicate code
+# def say_hello(name):
+#     print(f"Hello, {name}!")  # Duplicate code
 
 if __name__ == "__main__":
     connect_to_service()
